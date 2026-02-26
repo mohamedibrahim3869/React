22111eb47eb1000bfeffee31804bd000001b2388
mohamedibrahim3869 <mohamed.ibrahim3869@gmail.com>
2026-02-26 19:05:57 +0200

    new testing footer

---
+++ footer/src/App.jsx
@@ -0,0 +1,30 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li></li>
+                <li></li>
+                <p>©</p>
+                <a href="#">About</a>
+                <a href="#">Customize</a>
+                <a href="#">Copyrights</a>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <p>©</p>
+                <a href="#">About</a>
+                <a href="#">Customize</a>
+                <a href="#">Copyrights</a>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <p>©</p>
+                <a href="#">About</a>
+                <a href="#">Customize</a>
+                <a href="#">Copyrights</a>
+            </ul>
+        </footer>
+    );
+};
