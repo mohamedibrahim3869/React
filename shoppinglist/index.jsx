9a98d25a668620370cf2de8848bd6ebb3cb6a308
mohamedibrahim <mohamed.ibrahim3869@gmail.com>
2026-03-03 02:21:00 +0200

    new modifications

--- shoppinglist/index.jsx
+++ shoppinglist/index.jsx
@@ -1,15 +1,42 @@
 const { useState } = React;
 
-const items = ["Apples", "Bananas", "Strawberries", "Blueberries", "Mangoes", "Pineapple", "Lettuce", "Broccoli", "Paper Towels", "Dish Soap"];
+const items = [
+    "Apples",
+    "Bananas",
+    "Strawberries",
+    "Blueberries",
+    "Mangoes",
+    "Pineapple",
+    "Lettuce",
+    "Broccoli",
+    "Paper Towels",
+    "Dish Soap",
+];
 export const ShoppingList = () => {
-  return (
-    <div className="container">
-      <h1>Shopping List</h1>
-      <form>
-        <label htmlFor="search">Search for an item:</label> 
-        <input type="search" id="search" placeholder="Search..." aria-describedby="search-description"/>
-        <p id="search-description">Type to filter the list below:</p>
-      </form>
-    </div>
-  );
+    const filteredItems = items.filter((item) => {
+        item.toLowerCase();
+    });
+    const [query, setQuery] = useState("");
+    return (
+        <div className="container">
+            <h1>Shopping List</h1>
+            <form>
+                <label htmlFor="search">Search for an item:</label>
+                <input
+                    type="search"
+                    id="search"
+                    placeholder="Search..."
+                    aria-describedby="search-description"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <p id="search-description">Type to filter the list below:</p>
+                <ul>
+                    {filteredItems.map((item) => {
+                        <li key={item}>{item}</li>;
+                    })}
+                </ul>
+            </form>
+        </div>
+    );
 };
