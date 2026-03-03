c1b30b12a5326a1b534b581adaa4c8ae2c01b7d6
mohamedibrahim <mohamed.ibrahim3869@gmail.com>
2026-03-03 20:02:23 +0200

    finished the project

--- shoppinglist/index.jsx
+++ shoppinglist/index.jsx
@@ -1,4 +1,4 @@
-const { useState } = React;
+const { useState, useMemo , useCallback} = React;
 
 const items = [
     "Apples",
@@ -12,19 +12,35 @@ const items = [
     "Paper Towels",
     "Dish Soap",
 ];
+let prevToggleItem = null;
 export const ShoppingList = () => {
-    const filteredItems = items.filter((item) => {
-        item.toLowerCase();
-    });
     const [query, setQuery] = useState("");
+    const [selectedItems, setSelectedItems] = useState([]);
+
+    const filteredItems = useMemo( () => {
+        console.log("Filtering items...");
+        return items.filter((item) =>
+        item.toLowerCase().includes(query.toLowerCase()),
+    );
+    }, [query])
+    const toggleItem = useCallback((item) => {
+        setSelectedItems((prev) => prev.includes(item) ? prev.filter(i => i!== item): [...prev, item])
+    }) 
+    if (prevToggleItem !== toggleItem) {
+    console.log("New toggleItem function");
+    prevToggleItem = toggleItem;
+  } else {
+        console.log("Current toggleItem function")
+    }
+
     return (
         <div className="container">
             <h1>Shopping List</h1>
             <form>
                 <label htmlFor="search">Search for an item:</label>
                 <input
-                    type="search"
                     id="search"
+                    type="search"
                     placeholder="Search..."
                     aria-describedby="search-description"
                     value={query}
@@ -33,7 +49,24 @@ export const ShoppingList = () => {
                 <p id="search-description">Type to filter the list below:</p>
                 <ul>
                     {filteredItems.map((item) => {
-                        <li key={item}>{item}</li>;
+                        const isChecked = selectedItems.includes(item);
+                        return (
+                            <li
+                                key={item}
+                                style={{
+                                    textDecoration:
+                                        isChecked ? "line-through" : "none",
+                                }}>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        onChange={() => toggleItem(item)}
+                                        checked={isChecked}
+                                    />
+                                    {item}
+                                </label>
+                            </li>
+                        );
                     })}
                 </ul>
             </form>
